0407a4dfbf1c46ba9b592a6f0d587964e15286e6
zwf <zwf19980423@gmail.com>
2021-04-01 21:19:40 +0800

    card-compos & json-server delele method

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Container } from '@material-ui/core';
+import NoteCard from '../components/NoteCard';
 export default function Notes() {
   const [notes, setNotes] = useState([]);
   useEffect(() => {
@@ -10,6 +11,11 @@ export default function Notes() {
       .then(data => setNotes(data));
   }, []);
 
+  const handleDelete = async id => {
+    await fetch('http://localhost:8000/notes/' + id, { method: 'DELETE' });
+    const newNotes = notes.filter(note => note.id !== id);
+    setNotes(newNotes);
+  };
   return (
     <Container>
       {/* <Grid container>
@@ -26,10 +32,10 @@ export default function Notes() {
           <Paper>4</Paper>
         </Grid>
       </Grid> */}
-      <Grid container>
+      <Grid container spacing={3}>
         {notes.map(note => (
           <Grid item key={note.id} xs={12} md={6} lg={4}>
-            <Paper>{note.title}</Paper>
+            <NoteCard note={note} handleDelete={handleDelete} />
           </Grid>
         ))}
       </Grid>
