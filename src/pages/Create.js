7c6a7109cdc8b6f8ed0256db4c6bb82077c614d9
zwf <zwf19980423@gmail.com>
2021-04-01 19:44:59 +0800

    custome theme

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,27 +2,13 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core';
+
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: 'violet',
-    '&:hover': {
-      backgroundColor: 'blue',
-    },
-  },
-  title: {
-    textDecoration: 'underline',
-    marginBottom: 20,
-  },
-});
+
 export default function Create() {
-  const classes = useStyles();
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant="h6"
         component="h2"
         color="textSecondary"
@@ -31,7 +17,6 @@ export default function Create() {
         Create a New Note
       </Typography>
       <Button
-        className={classes.btn}
         onClick={() => {
           console.log('you cliked me');
         }}
