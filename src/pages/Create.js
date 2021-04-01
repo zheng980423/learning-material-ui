e530b821f697f64d7abefe1c8db70cbc08593d7f
zwf <zwf19980423@gmail.com>
2021-04-01 18:35:35 +0800

    typography comps

---
+++ src/pages/Create.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+export default function Create() {
+  return (
+    <div>
+      {/* <Typography variant="h1" color="primary" align="center">
+        Create page
+      </Typography> */}
+      {/* <Typography color="secondary" noWrap>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore
+        cupiditate non, vero sunt, adipisci dignissimos amet et animi quam
+        pariatur distinctio, cum saepe sint sit.
+      </Typography> */}
+      {/* https://material-ui.com/zh/api/typography/ */}
+    </div>
+  );
+}
