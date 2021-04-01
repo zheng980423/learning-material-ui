861a65874e8a4c8c8cfaae01d99a9bd2bcd39581
zwf <zwf19980423@gmail.com>
2021-04-01 18:50:49 +0800

    button- comps

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+// import ButtonGroup from '@material-ui/core/ButtonGroup';
+
 export default function Create() {
   return (
-    <div>
+    <Container>
       {/* <Typography variant="h1" color="primary" align="center">
         Create page
       </Typography> */}
@@ -20,6 +24,27 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-    </div>
+      <Button
+        onClick={() => {
+          console.log('you cliked me');
+        }}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+      {/* <Button type="submit" color="default">
+        Submit
+      </Button>
+      <Button type="submit" color="secondary" variant="outlined">
+        Submit
+      </Button>
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   );
 }
