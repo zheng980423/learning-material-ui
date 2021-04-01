f35d9093fae9ec4a044bda7d8ee967d76cbb875c
zwf <zwf19980423@gmail.com>
2021-04-01 23:43:25 +0800

    masonry layout

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Container } from '@material-ui/core';
 import NoteCard from '../components/NoteCard';
+import Masonry from 'react-masonry-css';
 export default function Notes() {
   const [notes, setNotes] = useState([]);
   useEffect(() => {
@@ -16,29 +17,25 @@ export default function Notes() {
     const newNotes = notes.filter(note => note.id !== id);
     setNotes(newNotes);
   };
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
+
   return (
     <Container>
-      {/* <Grid container>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>4</Paper>
-        </Grid>
-      </Grid> */}
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {notes.map(note => (
-          <Grid item key={note.id} xs={12} md={6} lg={4}>
+          <div key={note.id}>
             <NoteCard note={note} handleDelete={handleDelete} />
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   );
 }
