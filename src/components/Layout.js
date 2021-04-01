d7a35795153681ecead26ca4cff80838a1a199e4
zwf <zwf19980423@gmail.com>
2021-04-01 23:32:14 +0800

    avatar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,7 @@ import { useHistory, useLocation } from 'react-router';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { format } from 'date-fns';
+import Avatar from '@material-ui/core/Avatar';
 const drawWidth = 240;
 const useStyles = makeStyles(theme => {
   return {
@@ -35,6 +36,9 @@ const useStyles = makeStyles(theme => {
     },
     toolbar: theme.mixins.toolbar,
     date: { flexGrow: 1 },
+    avatar: {
+      marginLeft: theme.spacing(2),
+    },
   };
 });
 const Layout = ({ children }) => {
@@ -61,6 +65,7 @@ const Layout = ({ children }) => {
             Today is the {format(new Date(), 'do MMMM Y')}
           </Typography>
           <Typography>Mario</Typography>
+          <Avatar src="/mario-av.png" className={classes.avatar} />
         </Toolbar>
       </AppBar>
       <Drawer
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,13 +2,29 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import { IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Avatar, IconButton, makeStyles, Typography } from '@material-ui/core';
 import { DeleteOutlined } from '@material-ui/icons';
+import { blue, green, pink, yellow } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
   test: {
     border: note => {
       if (note.category === 'work') return '1px solid red';
+    },
+  },
+  avatar: {
+    backgroundColor: note => {
+      if (note.category === 'work') {
+        return yellow[700];
+      }
+      if (note.category === 'money') {
+        return green[500];
+      }
+      if (note.category === 'todos') {
+        return pink[500];
+      }
+
+      return blue[500];
     },
   },
 });
@@ -18,6 +34,11 @@ const NoteCard = ({ note, handleDelete }) => {
     <div>
       <Card elevation={1} className={classes.test}>
         <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
           action={
             <IconButton
               onClick={() => {
