81a122f875ef077c11a604482071a102cb9f1c40
zwf <zwf19980423@gmail.com>
2021-04-01 19:07:47 +0800

    custom css makeStyles

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,13 +2,27 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import SendIcon from '@material-ui/icons/Send';
-
+import { makeStyles } from '@material-ui/core';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue',
+    },
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20,
+  },
+});
 export default function Create() {
+  const classes = useStyles();
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         component="h2"
         color="textSecondary"
@@ -17,6 +31,7 @@ export default function Create() {
         Create a New Note
       </Typography>
       <Button
+        className={classes.btn}
         onClick={() => {
           console.log('you cliked me');
         }}
@@ -27,14 +42,6 @@ export default function Create() {
       >
         Submit
       </Button>
-      {/* icons */}
-      <br />
-      {/* <AcUnitOutlinedIcon color="secondary" fontSize="large" />
-      <AcUnitOutlinedIcon />
-      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
-      <AcUnitOutlinedIcon color="action" fontSize="small" />
-      <AcUnitOutlinedIcon color="error" fontSize="small" />
-      <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
     </Container>
   );
 }
